5854e0d72cfa2782539181caa3ca095a8cc06a72
Valk <sebastianbelle074@protonmail.com>
2025-06-14 23:26:53 -0400

    Change several props to be init

--- Utils/TileInfo.cs
+++ Utils/TileInfo.cs
@@ -2,19 +2,22 @@
 
 public class TileInfo
 {
-    public int WallType { get; set; }
+    // Init properties
+    public int WallType { get; init; }
+    public int LiquidType { get; init; }
+    public int Style { get; init; }
+    public byte LiquidAmount { get; init; }
+    public byte TileColor { get; init; }
+    public byte WallColor { get; init; }
+    public int TileFrameX { get; init; }
+    public int TileFrameY { get; init; }
+    public int Slope { get; init; }
+    public bool HasTile { get; init; }
+    public bool IsReplaceTile { get; init; }
+    
+    // Properties that have get AND set
     public int TileType { get; set; }
-    public int LiquidType { get; set; }
-    public int Style { get; set; }
-    public byte LiquidAmount { get; set; }
-    public byte TileColor { get; set; }
-    public byte WallColor { get; set; }
-    public int TileFrameX { get; set; }
-    public int TileFrameY { get; set; }
-    public int Slope { get; set; }
-    public bool HasTile { get; set; }
-    public bool IsReplaceTile { get; set; }
 
     // Note: This property is not used when serialized
-    public Vector2I Position { get; set; }
+    public Vector2I Position { get; init; }
 }
