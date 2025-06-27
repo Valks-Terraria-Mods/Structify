e766cbbd35ae17f29f0e19918d3ec965c9f44f2d
Valk <sebastianbelle074@protonmail.com>
2025-06-27 15:01:12 -0400

    Structure by Valky

--- Utils/Builders.cs
+++ Utils/Builders.cs
@@ -14,7 +14,7 @@ public static class Builders
     {
         string[] builderNames =
         [
-            //Valkyrienyanko, // :P
+            // Valkyrienyanko, // :P
             Expination,
             Grim,
             Toast,
